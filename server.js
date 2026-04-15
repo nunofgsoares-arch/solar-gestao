f409a4a96252905515844617986d8e22d75626a4
nunofgsoares-arch <nunofgsoares@gmail.com>
2026-04-15 22:07:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -137,3 +137,9 @@ app.post("/subscribe", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
